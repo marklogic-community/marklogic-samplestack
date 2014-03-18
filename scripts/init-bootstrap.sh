287a3c610605663dae4be1e9f94fb2736aa343f2
Charles Greer <cgreer@marklogic.com>
2014-03-18 10:53:37 -0700

    ingestion of github data, search transform

--- scripts/init-bootstrap.sh
+++ scripts/init-bootstrap.sh
@@ -6,6 +6,6 @@ sudo /etc/init.d/MarkLogic start
 curl --anyauth -Hcontent-type:application/json -d'{}' -X POST http://localhost:8001/admin/v1/init
 curl --anyauth -Haccept:application/json http://localhost:8001/admin/v1/timestamp
 curl --anyauth -Hcontent-type:application/json -d'{ "admin-username" : "admin", "admin-password" : "admin", "realm" : "public" }' -X POST http://localhost:8001/admin/v1/instance-admin 
-curl --anyauth -user admin:admin -Haccept:application/json http://localhost:8001/admin/v1/timestamp
+curl --anyauth --user admin:admin -Haccept:application/json http://localhost:8001/admin/v1/timestamp
 curl --anyauth --user admin:admin -Hcontent-type:application/json -d'{"rest-api":{ "name" : "boo", "port" : 8006} }' -X POST http://localhost:8002/v1/rest-apis
 curl --anyauth --user admin:admin -X DELETE http://localhost:8002/v1/rest-apis/boo
---
+++ scripts/search.sh
@@ -0,0 +1,24 @@
+# straight ahead directory search to find just github stuff, language, with facet on type...
+curl --user admin:admin --digest -d@'src/main/resources/search1.json' -X POST -Hcontent-type:application/json -H accept:application/json "http://localhost:8006/v1/search?directory=/github/" | python -mjson.tool | less
+
+# without directory, with tranfsorm
+curl --user admin:admin --digest -d@'src/main/resources/search1.json' -X POST -Hcontent-type:application/json -H accept:application/json "http://localhost:8006/v1/search?transform=related"
+
+# string search, w transform
+curl --user admin:admin --digest -X GET -H accept:application/json "http://localhost:8006/v1/search?transform=related&q=language"  | python -mjson.tool
+
+# post a transform
+curl --user admin:admin --digest -d@'src/main/xquery/related-result-transform.xqy' -X PUT -Hcontent-type:application/xquery http://localhost:8006/v1/config/transforms/related
+
+# CRUD get a github doc
+curl --user admin:admin --digest -X GET -Haccept:application/json http://localhost:8006/v1/documents?uri=/github/1007227863139673301.json | python -mjson.tool
+
+
+things to consider:
+facets, co-occurrences on type and lanaguage (what languages are most often forked)
+
+transform results to get related things - 1. search for clojure, find things about clojure in skos.
+
+AHA
+if a search results is triple file, do result expansion in result transform...
+
