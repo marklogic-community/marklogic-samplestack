19855c25e4399888b251e42f414a922ec63fc8e3
Stu Salsbury <stu.salsbury@gmail.com>
2014-05-10 20:42:50 -0700

    chore(test): improve testHelper's facilities to stub promises
    `testHelper.stubPromiseDeferred` and `testHelper.stubPromiseValue`
    give greater control to tests when configuring stubbed promises.

--- test/unit-tests/testHelper.js
+++ test/unit-tests/testHelper.js
@@ -14,14 +14,45 @@
     return resp;
   };
 
-  helper.stubPromise = function ($q, theObj, methodName, value) {
+  /**
+   * Create/replace  a function on a stub that will return promise a promise.
+   * A provided handle is supplied with a deferred object that it can resolve.
+   * @param  {angular.$q} $q
+   * @param  {Object} stubObj Object on which to create the function,  If
+   * falsy, an object is generated.
+   * @param  {string} funcName Name of tghe function to create/replace
+   * @param  {Object} deferredHandle object on which to assign a deferreda.
+   * @return {Object} The stub.
+   */
+  helper.stubPromiseDeferred = function (
+    $q, stubObj, funcName, deferredHandle
+  ) {
     var deferred = $q.defer();
     var promise = deferred.promise;
-    var stub = sinon.stub(theObj, methodName).returns(promise);
-    deferred.resolve(value);
+    var stub = sinon.stub(stubObj, funcName).returns(promise);
+    deferredHandle.deferred = deferred;
     return stub;
   };
 
-  window.myTest = helper;
+  /**
+   * Create/replace  a function on a stub that will return promise a promise.
+   * The promise resolves to a specified value.
+   * @param  {angular.$q} $q
+   * @param  {Object} stubObj Object on which to create the function,  If
+   * falsy, an object is generated.
+   * @param  {string} funcName Name of tghe function to create/replace
+   * @param  {*} value The value to which the promise should reslove
+   * @return {Object} The stub.
+   */
+  helper.stubPromiseValue = function (
+    $q, stubObj, funcName, value
+  ) {
+    var dh = {};
+    var stub = helper.stubPromiseDeferred($q, stubObj, funcName, dh);
+    dh.resolve(value);
+    return stub;
+  };
 
-})(window.appContext);
+  this.testHelper = helper;
+
+}).call(global);
