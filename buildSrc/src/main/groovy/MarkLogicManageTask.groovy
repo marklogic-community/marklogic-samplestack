02ca75d76d0d059f3bc4c833245cba4ac0b789e6
Charles Greer <cgreer@marklogic.com>
2014-06-02 14:20:12 -0700

    renames, cleaning

--- buildSrc/src/main/groovy/MarkLogicManageTask.groovy
+++ buildSrc/src/main/groovy/MarkLogicManageTask.groovy
@@ -5,7 +5,7 @@ import org.gradle.api.tasks.TaskAction
 
 public class MarkLogicManageTask extends MarkLogicTask {
 
-    def config = "database/manage-properties.json"
+    def config = "database/database-properties.json"
 
     @TaskAction
     void updateDatabase() {
