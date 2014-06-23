c2ce50d78babed092e138e4f58273d547de550df
Mike Wooldridge <mike@hellion.org>
2014-06-22 23:49:11 -0700

    html/scss: semantics and wireframes
    Add html to implement main states of the application as per wireframes.
    
    Use semantic class names to define structure.
    
    Use scss, primarily @extend to connect html to bootstrap classes.
    
    Customize scss to match wireframes.

---
+++ browser/src/app/states/qnaDoc.js
@@ -0,0 +1,12 @@
+define(['app/module'], function (module) {
+
+  module.controller('qnaDocCtlr', [
+
+    '$scope',
+    function ($scope) {
+      $scope.setPageTitle('doc');
+    }
+
+  ]);
+
+});
