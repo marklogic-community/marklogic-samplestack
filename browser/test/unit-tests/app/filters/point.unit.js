ea5c442e455bb19d455fe2622e4e309e9e1f9010
Stu Salsbury <stu.salsbury@gmail.com>
2014-06-22 20:52:16 -0700

    chore: restore unit testing

--- browser/test/unit-tests/app/filters/point.unit.js
+++ browser/test/unit-tests/app/filters/point.unit.js
@@ -1,45 +1,48 @@
-describe('app/filters/point', function () {
-  var sut;
+define([], function () {
 
-  beforeEach(function () {
-    module('app');
-  });
+  describe('filters/point', function () {
+    var sut;
 
-  describe('toPointFilter', function () {
-
-    beforeEach(function (done) {
-      inject(
-        function (toPointFilter) {
-          sut = toPointFilter;
-          done();
-        }
-      );
+    beforeEach(function () {
+      angular.mock.module('app');
     });
 
-    it('should parse a string using parens to a point', function () {
-      sut('(3,2.8)').should.eql([3,2.8]);
+    describe('toPointFilter', function () {
+
+      beforeEach(function (done) {
+        inject(
+          function (toPointFilter) {
+            sut = toPointFilter;
+            done();
+          }
+        );
+      });
+
+      it('should parse a string using parens to a point', function () {
+        sut('(3,2.8)').should.eql([3,2.8]);
+      });
+
+      it('should parse a string using brackets to a point', function () {
+        sut('[0xF,70)').should.eql([15,70]);
+      });
+
     });
 
-    it('should parse a string using brackets to a point', function () {
-      sut('[0xF,70)').should.eql([15,70]);
+    describe('fromPointFilter', function () {
+
+      beforeEach(function (done) {
+        inject(
+          function (fromPointFilter) {
+            sut = fromPointFilter;
+            done();
+          }
+        );
+      });
+
+      it('should convert a point to a string', function () {
+        sut([3,2.8]).should.eql('[3,2.8]');
+      });
+
     });
-
-  });
-
-  describe('fromPointFilter', function () {
-
-    beforeEach(function (done) {
-      inject(
-        function (fromPointFilter) {
-          sut = fromPointFilter;
-          done();
-        }
-      );
-    });
-
-    it('should convert a point to a string', function () {
-      sut([3,2.8]).should.eql('[3,2.8]');
-    });
-
   });
 });
