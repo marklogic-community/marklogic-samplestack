f283c6e506d34ee42235ba6eca37a23cd3946118
Charles Greer <cgreer@marklogic.com>
2014-06-27 14:11:30 -0700

    related tags extension

---
+++ appserver/java-spring/src/test/java/com/marklogic/samplestack/db/RelatedTagsTest.java
@@ -0,0 +1,49 @@
+package com.marklogic.samplestack.db;
+
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.marklogic.samplestack.impl.DatabaseContext;
+import com.marklogic.samplestack.impl.RelatedTagsManager;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DatabaseContext.class)
+@Category(DatabaseExtensionTest.class)
+public class RelatedTagsTest {
+
+
+	private final Logger logger = LoggerFactory
+			.getLogger(RelatedTagsTest.class);
+
+	
+	@Autowired
+	RelatedTagsManager manager;
+	
+	@Test
+	@Ignore
+	public void testRelatedTags() {
+		String startTag = "javascript";
+		
+		List<String> relatedTags = manager.getRelatedTags(startTag);
+		
+		logger.info("" + relatedTags);
+		
+		
+		logger.info("" + manager.getRelatedTags("xquery"));
+		
+		fail("No assertions");
+		
+	}
+}
