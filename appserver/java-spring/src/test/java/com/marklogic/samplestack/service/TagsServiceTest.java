b223ed8ddf8fc6d6f0ef802c30ac0360353cea44
Charles Greer <cgreer@marklogic.com>
2014-07-07 15:31:33 -0700

    Tags for the no-query case
    closes #89

--- appserver/java-spring/src/test/java/com/marklogic/samplestack/service/TagsServiceTest.java
+++ appserver/java-spring/src/test/java/com/marklogic/samplestack/service/TagsServiceTest.java
@@ -15,7 +15,6 @@ import com.marklogic.client.io.JacksonHandle;
 import com.marklogic.samplestack.domain.ClientRole;
 import com.marklogic.samplestack.impl.DatabaseContext;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { DatabaseContext.class })
 @Category(MiddleTierIntegrationTest.class)
@@ -23,34 +22,55 @@ public class TagsServiceTest extends MarkLogicIntegrationTest {
 
 	@Autowired
 	TagsService tagsService;
-	
+
 	@Autowired
 	MarkLogicOperations operations;
-	
+
 	@Test
 	public void testTagSuggestion() {
 		JsonNode tagsJson = getTestJson("questions/tags.json");
-		
-		JSONDocumentManager docMgr = operations.newJSONDocumentManager(ClientRole.SAMPLESTACK_CONTRIBUTOR);
-		docMgr.write("/tags.json", new JacksonHandle(tagsJson) );
-		
-		String[] suggestions = tagsService.suggestTags(ClientRole.SAMPLESTACK_CONTRIBUTOR);
+
+		JSONDocumentManager docMgr = operations
+				.newJSONDocumentManager(ClientRole.SAMPLESTACK_CONTRIBUTOR);
+		docMgr.write("/tags.json", new JacksonHandle(tagsJson));
+
+		String[] suggestions = tagsService
+				.suggestTags(ClientRole.SAMPLESTACK_CONTRIBUTOR);
 		assertTrue("Need suggestions to test", suggestions.length > 0);
-		
-		suggestions = tagsService.suggestTags(ClientRole.SAMPLESTACK_CONTRIBUTOR, "ab");
+
+		suggestions = tagsService.suggestTags(
+				ClientRole.SAMPLESTACK_CONTRIBUTOR, "ab");
 		assertTrue("Need suggestions to test", suggestions.length > 0);
 
 		assertAllContain(suggestions, "ab");
-		
+
 		docMgr.delete("/tags.json");
-		
+
 	}
-	
+
+	@Test
+	public void testAllTags() {
+		JsonNode tagsJson = getTestJson("questions/tags.json");
+
+		JSONDocumentManager docMgr = operations
+				.newJSONDocumentManager(ClientRole.SAMPLESTACK_CONTRIBUTOR);
+		docMgr.write("/tags.json", new JacksonHandle(tagsJson));
+
+		String[] suggestions = tagsService
+				.suggestTags(ClientRole.SAMPLESTACK_CONTRIBUTOR);
+		assertTrue("Need suggestions to test", suggestions.length > 1);
+
+
+		docMgr.delete("/tags.json");
+
+	}
+
 	private void assertAllContain(String[] suggestions, String pattern) {
 		for (String suggestion : suggestions) {
-			assertTrue("Suggestion contains candidate", suggestion.contains(pattern));
+			assertTrue("Suggestion contains candidate",
+					suggestion.contains(pattern));
 		}
-		
+
 	}
-	
+
 }
