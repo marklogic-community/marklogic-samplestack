f86057bd795a27107c0da99f0973eea63d1acf2c
Stu Salsbury <stu.salsbury@gmail.com>
2014-07-08 13:51:43 -0700

    unit tests: organize suites

--- browser/test/unit-tests/all.unit.js
+++ browser/test/unit-tests/all.unit.js
@@ -1,2 +1,28 @@
 define([
-], function () {});
+], function (
+) {
+  describe('unit tests', function () {
+    describe('_marklogic', function () {
+      describe('directives', function () {
+
+      });
+      describe('services', function () {
+
+      });
+    });
+    describe('app', function () {
+      describe('dialogs', function () {
+
+      });
+      describe('directives', function () {
+
+      });
+      describe('services', function () {
+
+      });
+      describe('states', function () {
+
+      });
+    });
+  });
+});
