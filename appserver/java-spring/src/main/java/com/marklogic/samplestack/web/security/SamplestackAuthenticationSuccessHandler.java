59d7ce086a01a54e6690a95a4f71b68359359311
Charles Greer <cgreer@marklogic.com>
2014-07-15 10:20:00 -0700

    Role information in login payload

--- appserver/java-spring/src/main/java/com/marklogic/samplestack/web/security/SamplestackAuthenticationSuccessHandler.java
+++ appserver/java-spring/src/main/java/com/marklogic/samplestack/web/security/SamplestackAuthenticationSuccessHandler.java
@@ -1,12 +1,14 @@
 package com.marklogic.samplestack.web.security;
 
 import java.io.IOException;
+import java.io.Writer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -15,11 +17,18 @@ import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.marklogic.samplestack.domain.ClientRole;
+
 @Component
 // credit to TODO
 // http://www.baeldung.com/2011/10/31/securing-a-restful-web-service-with-spring-security-3-1-part-3/
 public class SamplestackAuthenticationSuccessHandler extends
 		SimpleUrlAuthenticationSuccessHandler {
+
+	@Autowired
+	private ObjectMapper mapper;
 
 	private RequestCache requestCache = new HttpSessionRequestCache();
 
@@ -28,23 +37,23 @@ public class SamplestackAuthenticationSuccessHandler extends
 			HttpServletResponse response, Authentication authentication)
 			throws ServletException, IOException {
 		SavedRequest savedRequest = requestCache.getRequest(request, response);
-
-		if (savedRequest == null) {
-			clearAuthenticationAttributes(request);
-			return;
-		}
 		String targetUrlParam = getTargetUrlParameter();
-		if (isAlwaysUseDefaultTargetUrl()
+		if (savedRequest != null
+				&& isAlwaysUseDefaultTargetUrl()
 				|| (targetUrlParam != null && StringUtils.hasText(request
 						.getParameter(targetUrlParam)))) {
 			requestCache.removeRequest(request, response);
-			clearAuthenticationAttributes(request);
-			return;
 		}
 
 		clearAuthenticationAttributes(request);
 		HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(
 				response);
+		Writer writer = responseWrapper.getWriter();
+		ObjectNode userNode = mapper.createObjectNode();
+		userNode.put("userName", ClientRole.securityContextUserName());
+		userNode.put("role", ClientRole.securityContextRole().toString());
+		mapper.writeValue(writer, userNode);
+		writer.close();
 	}
 
 	public void setRequestCache(RequestCache requestCache) {
