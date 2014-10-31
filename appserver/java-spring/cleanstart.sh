206798d365b1a5d03065fef9bcad02888f1da688
Stu Salsbury <stu.salsbury@gmail.com>
2014-10-31 15:27:07 -0700

    java: convenience .sh script for full gradle run
    cleanstart.sh downloads the current seed data and does a full, clean
    setup of the Samplestack Java tier and finishes by launching bootrun.
    
    It is not expected to work on Windows. In the future, a cross-platform
    solution may be developed in gradle.
    
    For now, it represents the expected path for successful launch of the
    Java stack after retrieving the code from git (minus all browser-related
    commands).
    
    It should work for a clean install and for a reinstall.

---
+++ appserver/java-spring/cleanstart.sh
@@ -0,0 +1,13 @@
+echo "Enter <username:password> (e.g. ssalsbur:notMyPassword) for wiki.marklogic.com:"
+read -s creds
+rm -rf ../../database/seed-data
+curl -L -u $creds -o seed.tgz https://wiki.marklogic.com/download/attachments/31359376/seed-data1.3.tgz?api=v2 -o seed.tgz
+tar -zxvf seed.tgz -C ../..
+rm -rf seed.tgz
+./gradlew dbteardown
+./gradlew clean
+./gradlew dbinit
+./gradlew dbconfigure
+./gradlew test
+./gradlew dbload
+./gradlew bootrun
