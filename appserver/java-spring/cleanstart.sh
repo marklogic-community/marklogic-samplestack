efeb34dec3c98cee18e972fdc44ca2afca46f133
Stu Salsbury <stu.salsbury@gmail.com>
2014-11-03 10:27:10 -0800

    feat(java/cleanstart.sh): use ConfigureClean, ignore server cert
    Add dbConfigureClean task to the cleanstart.sh script.
    
    Allow curl to ignore certificate trust issues for seed-data download.

--- appserver/java-spring/cleanstart.sh
+++ appserver/java-spring/cleanstart.sh
@@ -1,10 +1,11 @@
 echo "Enter <username:password> (e.g. ssalsbur:notMyPassword) for wiki.marklogic.com:"
 read -s creds
 rm -rf ../../database/seed-data
-curl -L -u $creds -o seed.tgz https://wiki.marklogic.com/download/attachments/31359376/seed-data1.3.tgz?api=v2 -o seed.tgz
+curl -k -L -u $creds -o seed.tgz https://wiki.marklogic.com/download/attachments/31359376/seed-data1.3.tgz?api=v2 -o seed.tgz
 tar -zxvf seed.tgz -C ../..
 rm -rf seed.tgz
 ./gradlew dbteardown
+./gradlew dbConfigureClean
 ./gradlew clean
 ./gradlew dbinit
 ./gradlew dbconfigure
