3f759e448dbe3468d1b257694ad8b78042733172
Stu Salsbury <stu.salsbury@gmail.com>
2014-11-06 09:40:02 -0800

    java: improve cleanstart.sh
    Use curl/mlvm to install MarkLogic nightly. User may specify nightly
    date as parameter or it will attempt to use today's nightly. Fails
    if nightly not found.
    
    Various workarounds for environmental instability.
    
    TODO: remove MarkLogic Data directory in Linux.

--- appserver/java-spring/cleanstart.sh
+++ appserver/java-spring/cleanstart.sh
@@ -1,10 +1,77 @@
-echo "Enter <username:password> (e.g. ssalsbur:notMyPassword) for wiki.marklogic.com:"
-read -s creds
+#!/bin/bash
+
+day=$(date +"%Y%m%d")
+test $1 && day=$1
+ver="8.0-$day"
+
+echo "********* Will be using MarkLogic nightly $ver"
+
+mlvmVers=($(mlvm list))
+install=1
+for i in "${mlvmVers[@]}"
+do
+  if [[ "$i" == "$ver" ]]; then
+    echo "********* Existing MLVM installation of $ver found; skipping download/install"
+    install=0
+  fi
+done
+
+OSTYPE=`uname`
+if [[ "$OSTYPE" == "Darwin" ]]; then
+  echo "********* OS is OSX"
+  fname="$ver.x86_64.dmg"
+  url="http://root.marklogic.com/nightly/builds/macosx-64/osx-intel64-80-build.marklogic.com/HEAD/pkgs.$day/MarkLogic-8.0-$day-x86_64.dmg"
+else
+  fname="$ver.x86_64.rpm"
+  url="http://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/HEAD/pkgs.$day/MarkLogic-8.0-$day.x86_64.rpm"
+fi
+if [[ $install == 1 ]]; then
+  echo "********* Downloading MarkLogic nightly $ver"
+
+  echo "********* Enter user for root.marklogic.com"
+  read -s user
+  echo "********* Enter password for $user on root.marklogic.com"
+  read -s pass
+
+  status=$(curl -k --anyauth -u $user:$pass --head --write-out %{http_code} --silent --output /dev/null $url)
+  if [[ $status = 200 ]]; then
+    curl -k --anyauth -u $user:$pass -o ./$fname $url
+    mlvm stop
+    mlvm install $fname $ver
+    rm -rf ./$fname
+    echo "********* MarkLogic nightly $ver installed to mlvm"
+  else
+    echo "CANNOT DOWNLOAD: status = $status for date $day (URL=\"$url\")"
+    exit 1
+  fi
+fi
+
+echo "********* Cleaning Up Gradle environment"
+rm -rf ./.gradle/2.1/taskArtifacts/*
+./gradlew --stop
+rm -rf ./.gradle/2.1/taskArtifacts/*
+
+echo "********* Downloading/Unpacking Seed Data"
 rm -rf ../../database/seed-data
-curl -k -L -u $creds -o seed.tgz https://wiki.marklogic.com/download/attachments/31359376/seed-data1.3.tgz?api=v2 -o seed.tgz
-tar -zxvf seed.tgz -C ../..
+curl -k -L -o seed.tgz http://developer.marklogic.com/media/gh/seed-data1.3.tgz -o seed.tgz
+tar -zxf seed.tgz -C ../..
 rm -rf seed.tgz
+
+mlvm stop
+if [[ "$OSTYPE" == "Darwin" ]]; then
+  echo "********* Reinitializing MarkLogic Installation"
+  rm -rf ~/Library/Application\ Support/MarkLogic/Data
+else
+  echo "*****************************************************"
+  echo "I don't know where linux ML stores data directory!!!!"
+  echo "Thus, not wiping out existing ML data"
+  echo "*****************************************************"
+fi
+
+mlvm use $ver
 ./gradlew dbteardown
+mlvm stop
+mlvm start
 ./gradlew dbConfigureClean
 ./gradlew clean
 ./gradlew dbinit
