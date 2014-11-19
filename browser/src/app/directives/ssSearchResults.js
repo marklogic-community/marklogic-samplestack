ce69d3f811fb8870fb20565d0cea6c9e8b4cc573
Stu Salsbury <stu.salsbury@gmail.com>
2014-11-19 04:33:43 -0800

    browser: change "score" to "votes" in UI
    "score" is confusing in a MarkLogic demo app because it has a specific
    meaning with respect to search relevance. Use "votes" so that it is
    clear that this isn't a relevance score.

--- browser/src/app/directives/ssSearchResults.js
+++ browser/src/app/directives/ssSearchResults.js
@@ -35,7 +35,7 @@ define(['app/module'], function (module) {
                 value: ['active']
               },
               {
-                label: 'score',
+                label: 'votes',
                 value: ['score']
               }
             ];
