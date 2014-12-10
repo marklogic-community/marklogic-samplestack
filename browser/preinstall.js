72d60c86c7b80cffd00395d44db507e03f8229bb
Stu Salsbury <stu.salsbury@gmail.com>
2014-12-09 23:13:59 -0800

    browser: do not pre-load lodash anymore
    Now that sauce-connect-launcher is fixed, do not pre-download
    lodash in a preinstall script.

--- browser/preinstall.js
+++
@@ -1,5 +0,0 @@
-var lodashVersion = require('./package.json').devDependencies.lodash
-    .replace(/[^\.\d]*/g, '');
-
-var cp = require('child_process')
-    .exec('npm install lodash@' + lodashVersion);
