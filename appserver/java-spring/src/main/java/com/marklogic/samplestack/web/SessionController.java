8f54db1bf9610bddc37d25c2529496d3e42ecab2
Charles Greer <cgreer@marklogic.com>
2014-12-10 11:12:33 -0800

    Dont return new vote structure in session info, return voteCount instead

--- appserver/java-spring/src/main/java/com/marklogic/samplestack/web/SessionController.java
+++ appserver/java-spring/src/main/java/com/marklogic/samplestack/web/SessionController.java
@@ -87,6 +87,8 @@ public class SessionController {
 				userNode = mapper.createObjectNode();
 				userNode.put("userName", userName);
 			}
+			userNode.put("voteCount", contributor.getVotes().size());
+			userNode.remove("votes");
 			ArrayNode roleNode = userNode.putArray("role");
 			roleNode.add(ClientRole.securityContextRole().toString());
 			return userNode;
