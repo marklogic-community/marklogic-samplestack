ac31ea332c9c4c14fe73f9f8b34321119dbc8941
Stu Salsbury <stu.salsbury@gmail.com>
2014-12-18 18:45:40 -0800

    browser(ssMarkdown): increase test timeout
    Increase the timeout for ssMarkdown test from 150ms to 300ms.
    
    Closes #378

--- browser/test/unit-tests/app/directives/ssMarkdown.unit.js
+++ browser/test/unit-tests/app/directives/ssMarkdown.unit.js
@@ -27,7 +27,7 @@ define(['testHelper'], function (helper) {
       it(
         'should call highlight on code',
           function () {
-            this.timeout(150);
+            this.timeout(300);
             scope.content = '# I Heading\n\n' +
                 '```javascript\nreturn \'I Code\';\n```\n';
             scope.$apply();
