deaf63c3954035165ff6e49e49e481c72d39fa0f
Daphne Maddox <laurelnaiad@gmail.com>
2015-01-11 18:07:38 -0800

    e2e: include java unit tests in travis on push

--- browser/dev-tasks/e2e/middleTierJava.js
+++ browser/dev-tasks/e2e/middleTierJava.js
@@ -92,6 +92,7 @@ var start = function (args, cb) {
     shellCmd.bind(null, dirForMiddle, gradleCmd + ' dbTeardown', null),
     shellCmd.bind(null, dirForMiddle, gradleCmd + ' dbInit', null),
     shellCmd.bind(null, dirForMiddle, gradleCmd + ' dbConfigure', null),
+    shellCmd.bind(null, dirForMiddle, gradleCmd + ' test', null),
     shellCmd.bind(null, dirForMiddle, gradleCmd + ' dbLoad', null),
     shellCmd.bind(
       null,
