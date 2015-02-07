6e57c7b312220239e7990f60e10ceae27ba278d3
Daphne Maddox <laurelnaiad@gmail.com>
2015-02-07 11:20:36 -0800

    e2e: use ML 8.0-1 in travis
    In travis, install/use ML release version rather than nightly.
    
    BREAKING CHANGE:
    
    Requires new environment variable ML_RELEASE_URL to be set to a URL
    which returns the .rpm file for the server, e.g.:
    
    `export ML_RELEASE_URL=http://developer.marklogic.com/download/binaries/8.0/MarkLogic-8.0-1.x86_64.rpm?t=INSERTKEYHERE&email=FIRSTNAME.LASTNAME%40EXAMPLE.COM`

--- shared/js/dev-tasks/e2e/travis-install-ml.sh
+++ shared/js/dev-tasks/e2e/travis-install-ml.sh
@@ -11,39 +11,73 @@ function successOrExit {
     fi
 }
 
-# find today
-day=$(date +"%Y%m%d")
-# if the user passed a day string as a param then use it instead
-test $1 && day=$1
-# make a version number out of the date
-ver="8.0-$day"
+test $1 && arg1=$1
+if [[ $arg1 = 'release' ]]; then
 
-echo "********* Will be using MarkLogic nightly $ver"
+  fname=MarkLogic-8.0-1.x86_64.rpm
+  ver=8.0-1
+  fnamedeb="marklogic_"
+  fnamedeb=$fnamedeb$ver
+  suff="_amd64.deb"
+  fnamedeb=$fnamedeb$suff
 
-# fetch/install ML nightly
-fname="MarkLogic-$ver.x86_64.rpm"
-fnamedeb="marklogic_"
-fnamedeb=$fnamedeb$ver
-suff="_amd64.deb"
-fnamedeb=$fnamedeb$suff
+  url=$MLRELEASE_URL
 
-url="http://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/HEAD/pkgs.$day/$fname"
-echo "********* Downloading MarkLogic nightly $ver"
+  echo "********* Downloading MarkLogic $ver"
 
+  status=$(curl -k --head --write-out %{http_code} --silent --output /dev/null $url)
+  if [[ $status = 200 ]]; then
+    successOrExit curl -k -o ./$fname $url
 
-status=$(curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD --head --write-out %{http_code} --silent --output /dev/null $url)
-if [[ $status = 200 ]]; then
-  successOrExit curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD -o ./$fname $url
+    fname=$(pwd)/$fname
 
-  fname=$(pwd)/$fname
+    sudo apt-get update
+    sudo apt-get install alien dpkg-dev debhelper build-essential
+    sudo alien -d -k $fname
+    sudo dpkg -i $fnamedeb
 
-  sudo apt-get update
-  sudo apt-get install alien dpkg-dev debhelper build-essential
-  sudo alien -d -k $fname
-  sudo dpkg -i $fnamedeb
+    echo "********* MarkLogic nightly $ver installed"
+  else
+    echo "CANNOT DOWNLOAD: status = $status for version $ver (URL=secure/suppressed)"
+    exit 1
+  fi
 
-  echo "********* MarkLogic nightly $ver installed"
 else
-  echo "CANNOT DOWNLOAD: status = $status for date $day (URL=\"$url\")"
-  exit 1
+  # find today
+  day=$(date +"%Y%m%d")
+
+  # if the user passed a day string as a param then use it instead
+  test $1 && day=$1
+  # make a version number out of the date
+  ver="8.0-$day"
+
+  echo "********* Will be using MarkLogic nightly $ver"
+
+  # fetch/install ML nightly
+  fname="MarkLogic-$ver.x86_64.rpm"
+  fnamedeb="marklogic_"
+  fnamedeb=$fnamedeb$ver
+  suff="_amd64.deb"
+  fnamedeb=$fnamedeb$suff
+
+  url="http://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/HEAD/pkgs.$day/$fname"
+  echo "********* Downloading MarkLogic nightly $ver"
+
+
+  status=$(curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD --head --write-out %{http_code} --silent --output /dev/null $url)
+  if [[ $status = 200 ]]; then
+    successOrExit curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD -o ./$fname $url
+
+    fname=$(pwd)/$fname
+
+    sudo apt-get update
+    sudo apt-get install alien dpkg-dev debhelper build-essential
+    sudo alien -d -k $fname
+    sudo dpkg -i $fnamedeb
+
+    echo "********* MarkLogic nightly $ver installed"
+  else
+    echo "CANNOT DOWNLOAD: status = $status for date $day (URL=\"$url\")"
+    exit 1
+  fi
 fi
