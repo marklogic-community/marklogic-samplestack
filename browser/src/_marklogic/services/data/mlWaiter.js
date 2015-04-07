b9bb7c6c50d20a7e35f422f67a24de39d9d532ee
Daphne Maddox <laurelnaiad@gmail.com>
2015-04-07 10:24:01 -0700

    js: add copyright text
    - run copyright task
    - modify top level .html files and dialog .html files so that comment
    don't leak up to too-high a level.
    
    Closes #365

--- browser/src/_marklogic/services/data/mlWaiter.js
+++ browser/src/_marklogic/services/data/mlWaiter.js
@@ -1,3 +1,19 @@
+/* 
+ * Copyright 2012-2015 MarkLogic Corporation 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */ 
+
 /*
 this is provisional -- to be refactored to follow basic patterns that
 Angular 2.0 is pointing towards where we're doing the same thing they are
