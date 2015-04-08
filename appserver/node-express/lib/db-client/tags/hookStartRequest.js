47fe03c1cdf8d0e3581c35a493f83e1ccd7c366c
Scott D Brooks <clockworked24.7@gmail.com>
2015-04-08 15:15:35 -0700

    fix: tags dialogue initial load fix
     * Fixes issue with All Tags dialogue loading empty
     * Fixes error seeing when empty dialogue closed
    
    Closes #641

--- appserver/node-express/lib/db-client/tags/hookStartRequest.js
+++ appserver/node-express/lib/db-client/tags/hookStartRequest.js
@@ -1,18 +1,18 @@
-/* 
- * Copyright 2012-2015 MarkLogic Corporation 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
- */ 
+/*
+ * Copyright 2012-2015 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // TODO In 8.0-1, CANNOT include parse* information with Node Client values
 // call, thus we can't get tags constrained to current search qtext
@@ -30,7 +30,7 @@ mlrest.startRequest = function (operation) {
   if (
     operation.options.path === '/v1/search?format=json&category=content' &&
     operation.requestBody && operation.requestBody.search &&
-    (operation.requestBody.search.forTag ||
+    (operation.requestBody.search.forTag !== undefined ||
     operation.requestBody.search.relatedTo)
   ) {
     var valuesName = _.deepGet(
