79bbec0ac819466f2342d26265d5edd20e63eec4
Scott D Brooks <clockworked24.7@gmail.com>
2015-04-16 14:28:36 -0700

    fix: test failure work-around
    Maximize browser window when testing to avoid Bootstrap responsive layout issues attributed to test failures.

--- browser/test/cucumber-support/World.js
+++ browser/test/cucumber-support/World.js
@@ -1,18 +1,18 @@
-/* 
- * Copyright 2012-2015 MarkLogic Corporation 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
- */ 
+/*
+ * Copyright 2012-2015 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // this file adds global variables -- not generally a good idea but for
 // cucumber tests it's handy
@@ -37,6 +37,7 @@ var currentPage;
 browser.getCapabilities().then(function (cap) {
   browser.caps = cap.caps_;
 });
+browser.driver.manage().window().maximize();
 
 function World (callback) {
   self = this;
